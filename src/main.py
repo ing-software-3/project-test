92d64007edb118a27a3f03f51b9d25eadfaef3fe
KrozFu <carlosuefagh@gmail.com>
2026-05-26 20:11:00 -0500

    chore: initializacion project

---
+++ src/main.py
@@ -0,0 +1,14 @@
+def sum(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def mult(x, y):
+    return x * y
+
+
+def is_greater(number_1, number_2):
+    return number_1 > number_2
---
+++ src/tests/test_main.py
@@ -0,0 +1,13 @@
+from src.main import sum, sub, mult, is_greater
+
+
+def test_sum():
+    assert sum(2, 5) == 7
+
+
+def test_sub():
+    assert sub(5, 2) == 3
+
+
+def test_mult():
+    assert mult(5, 5) == 25
